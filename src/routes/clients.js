3975fab8d8fc0c7da21f06db09646aec5a39af0d
Angel Barajas <Barajasb15@gmail.com>
2022-05-14 20:57:30 -0500

    add: Detalles finales agregados en clientes y score

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -1,77 +1,91 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const mysqlConnection  = require('../database.js');
-
+const mysqlConnection = require("../database.js");
 
 // GET all Clientes
-router.get('/', (req, res) => {
-    var sqlcommand = 'SELECT * FROM TB_CLIENTE';
+router.get("/", (req, res) => {
+    var sqlcommand = "SELECT * FROM TB_CLIENTE";
     mysqlConnection.query(sqlcommand, (err, rows, fields) => {
-      if(err) throw err;
-      else {
-        res.json(rows);
-      }
+        if (err) throw err;
+        else {
+            res.json(rows);
+        }
     });
 });
-  
+
 //GET ONE CLIENTE
-router.get('/:id', (req, res) => {
-var {id} = req.params.id;
-var sqlcommand = 'SELECT * FROM TB_CLIENTE WHERE ID_CLIENTE = ?';
-mysqlConnection.query(sqlcommand,[id], (err, rows, fields) => {
-    if(err) throw err;
-    else {
-    res.json(rows);
-    }
-});
-});
-
-// //POST CLIENTE
-router.post('/', (req, res) =>{
-const {nombre,telefono,direccion} = req.body
-// var sqlcommand = INSERT INTO TB_CLIENTE(NOMBRE,TELEFONO,DIRECCION) VALUES '${nombre}', '${telefono}', '${direccion}';
-var sqlcommand = `INSERT INTO TB_CLIENTE(NOMBRE,TELEFONO,DIRECCION) VALUES '${nombre}', '${telefono}', '${direccion}'`;
-mysqlConnection.query(sqlcommand, (err, rows, fields) => {
-    if(err) throw err;
-    else {
-    sqlcommand = `INSERT INTO TB_SCORE VALUES 0`;
-    mysqlConnection.query(sqlcommand,(err,rows,fields)=>{
-        if(err) throw err;
-        else{
-        res.json({status: 'Cliente Insertado'});
+router.get("/:id", (req, res) => {
+    var { id } = req.params;
+    var sqlcommand = "SELECT * FROM TB_CLIENTE WHERE ID_CLIENTE = ?";
+    mysqlConnection.query(sqlcommand, [id], (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows);
         }
-    })
-    }
-});
+    });
 });
 
-// //UPDATE CLIENTE
-// router.put('/:id', (req,res) =>{
-// const {id} = req.params.id
+//POST CLIENTE
+router.post("/", (req, res) => {
+    const { id, nombre, telefono, direccion, id_usuario, id_score } = req.body;
+    var query = `
+SET @ID_CLIENTE = ?;
+SET @NOMBRE = ?;
+SET @TELEFONO = ?;
+SET @DIRECCION = ?;
+SET @ID_USUARIO = ?;
+SET @ID_SCORE = ?;
+CALL ClienAddorUpdate(@ID_CLIENTE, @NOMBRE, @TELEFONO,@DIRECCCION, @ID_USUARIO,@ID_SCORE);
+`;
+    mysqlConnection.query(
+        query,
+        [id, nombre, telefono, direccion, id_usuario, id_score],
+        (err, rows, fields) => {
+            if (err) throw err;
+            else {
+                res.send("Cliente Agregado");
+            }
+        }
+    );
+});
 
-// const {nombre,telefono,direccion} = req.body
-// var sqlcommand = UPDATE TB_CLIENTE SET NOMBRE = '${nombre}', TELEFONO = '${telefono}', DIRECCION = '${direccion}' WHERE ID_CLIENTE = ${id} ;
-// mysqlConnection.query(sqlcommand, (err, rows, fields) => {
-//     if(err) throw err;
-//     else {
-//     res.json({status: 'Cliente Modificado'});
-//     }
-// });
-// });
+//UPDATE CLIENTE
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, telefono, direccion, id_usuario, id_score } = req.body;
+    var query = `
+SET @ID_CLIENTE = ?;
+SET @NOMBRE = ?;
+SET @TELEFONO = ?;
+SET @DIRECCION = ?;
+SET @ID_USUARIO = ?;
+SET @ID_SCORE = ?;
+CALL ClienAddorUpdate(@ID_CLIENTE, @NOMBRE, @TELEFONO,@DIRECCCION, @ID_USUARIO,@ID_SCORE);
+`;
+    mysqlConnection.query(
+        query,
+        [id, nombre, telefono, direccion, id_usuario, id_score],
+        (err, rows, fields) => {
+            if (err) throw err;
+            else {
+                res.send("Cliente Agregado");
+            }
+        }
+    );
+});
 
-// //DELETE CLIENTE
-// router.delete('/:id',(req,res) =>{
-// const {id} = req.params.id
+//DELETE CLIENTE
+router.delete("/:id", (req, res) => {
+    const { id } = req.params.id;
 
-// var sqlcommand = DELETE FROM TB_CLIENTE WHERE ID_CLIENTE = '${id}'
-// mysqlConnection.query(sqlcommand, (err, rows, fields) => {
-//     if(err) throw err;
-//     else {
-//     res.json({status: 'Cliente Agregado'});
-//     }
-// });
+    var sqlcommand = `DELETE FROM TB_CLIENTE WHERE ID_CLIENTE = '${id}'`;
+    mysqlConnection.query(sqlcommand, (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json({ status: "Cliente Agregado" });
+        }
+    });
+});
 
-// });
-
-  module.exports = router;
+module.exports = router;
--- src/routes/scores.js
+++ src/routes/scores.js
@@ -1,64 +1,90 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const mysqlConnection  = require('../database.js');
+const mysqlConnection = require("../database.js");
 
 //GET ALL SCORE
-router.get('/', (req, res) => {
-    var sqlcommand = 'SELECT * FROM TB_SCORE';
+router.get("/", (req, res) => {
+    var sqlcommand = "SELECT * FROM TB_SCORE";
     mysqlConnection.query(sqlcommand, (err, rows, fields) => {
-      if(err) throw err;
-      else {
-        res.json(rows);
-      }
+        if (err) throw err;
+        else {
+            res.json(rows);
+        }
     });
-  });
+});
 
-  
-// //GET ONE SCORE 
-  router.get('/:id', (req, res) => {
-  
-    const { id } = req.params
-    console.log(id)
+//GET ONE SCORE
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
 
-    var sqlcommand = 'SELECT * FROM TB_SCORE WHERE ID_SCORE = ?';
-    mysqlConnection.query(sqlcommand,[id], (err, rows, fields) => {
-      if(err) throw err;
-      else {
-        res.json(rows);
-      }
+    var sqlcommand = "SELECT * FROM TB_SCORE WHERE ID_SCORE = ?";
+    mysqlConnection.query(sqlcommand, [id], (err, rows, fields) => {
+        if (err) throw err;
+        else {
+            res.json(rows);
+        }
     });
-  });
-  
-//   //PUT SCORE  
-  router.put('/:id', (req,res) => {
-  
-    const { id } = req.params
-  
-    let {puntuacion} = req.body
-  
-    var sqlcommand = `UPDATE TB_SCORE SET PUNTUACION  = ${puntuacion} WHERE ID_SCORE = ${id}` ;
-  
+});
+//POST SCORE
+
+router.put("/", (req, res) => {
+    const { id, puntuacion, colillas, puntos_totales, recompensas } = req.body;
+    var query = `
+  SET @ID_SCORE = ?;
+  SET @PUNTUACION = ?;
+  SET @COLILLAS = ?;
+  SET @PUNTOS_TOTALES = ?;
+  SET @RECOMPENSAS = ?;  
+  CALL ScoreAddorUpdate(@ID_CLIENTE, @PUNTUACION , @COLILLAS,@PUNTOS_TOTALES, @RECOMPENSAS);
+  `;
+    mysqlConnection.query(
+        query,
+        [id, puntuacion, colillas, puntos_totales, recompensas],
+        (err, rows, fields) => {
+            if (err) throw err;
+            else {
+                res.send("SCORE EDITADO");
+            }
+        }
+    );
+});
+
+//PUT SCORE
+router.put("/:id", (req, res) => {
+    const { puntuacion, colillas, puntos_totales, recompensas } = req.body;
+    const { id } = req.params;
+    var query = `
+  SET @ID_SCORE = ?;
+  SET @PUNTUACION = ?;
+  SET @COLILLAS = ?;
+  SET @PUNTOS_TOTALES = ?;
+  SET @RECOMPENSAS = ?;  
+  CALL ScoreAddorUpdate(@ID_CLIENTE, @PUNTUACION , @COLILLAS,@PUNTOS_TOTALES, @RECOMPENSAS);
+  `;
+    mysqlConnection.query(
+        query,
+        [id, puntuacion, colillas, puntos_totales, recompensas],
+        (err, rows, fields) => {
+            if (err) throw err;
+            else {
+                res.send("SCORE EDITADO");
+            }
+        }
+    );
+});
+
+///DELETE SCORE
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    var sqlcommand = `DELETE FROM TB_SCORE WHERE ID_SCORE = ${id}`;
     mysqlConnection.query(sqlcommand, (err, rows, fields) => {
-      if(err) throw err;
-      else {
-        res.json({status: 'Score Actualizado'});
-      }
-    }); 
-  })
-  
-//   //DELETE SCORE
-  router.delete('/:id',(req,res) =>{
-    const { id } = req.params
-  
-    var sqlcommand = `DELETE FROM TB_SCORE WHERE ID_SCORE = ${id}`
-    mysqlConnection.query(sqlcommand, (err, rows, fields) => {
-      if(err) throw err;
-      else {
-        res.json({status: 'SCORE Eliminado'});
-      }
+        if (err) throw err;
+        else {
+            res.json({ status: "SCORE Eliminado" });
+        }
     });
-  
-  });
+});
 
 module.exports = router;
